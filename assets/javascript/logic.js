b8739715904aa32055dbbd0778aca9d4aac5305e
Morrowii <sadler.m.ryan@gmail.com>
2018-06-24 21:39:04 -0400

    added sticky  footer, contact form, and scroll animations

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,6 +6,7 @@ $('body').on('click', '#home-button', function() {
     $('#profile-section').addClass('d-flex');
     $('#portfolio-section').addClass('d-none');
     $('#contact-section').addClass('d-none');
+    $('.content').css('opacity', 0);
 });
 
 $('body').on('click', '#portfolio-button', function() {
@@ -15,6 +16,7 @@ $('body').on('click', '#portfolio-button', function() {
     $('#profile-section').addClass('d-none');
     $('#portfolio-section').addClass('d-flex');
     $('#contact-section').addClass('d-none');
+    $('.content').css('opacity', 0);
 });
 
 $('body').on('click', '#contact-button', function() {
@@ -24,4 +26,11 @@ $('body').on('click', '#contact-button', function() {
     $('#profile-section').addClass('d-none');
     $('#portfolio-section').addClass('d-none');
     $('#contact-section').addClass('d-flex');
+    $('.content').css('opacity', 0);
+});
+
+$(document).scroll(function() {
+  if (window.scrollY >= 0) {
+    $('.content').css('opacity', 1 - 50 / window.scrollY);
+  }
 });
