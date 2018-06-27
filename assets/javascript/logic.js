31082febfdd155e1e3f067a2d5b1ed6f107199e8
Morrowii <sadler.m.ryan@gmail.com>
2018-06-26 21:01:21 -0400

    added active link colors

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,31 +1,31 @@
 
 $('body').on('click', '#home-button', function() {
-    $('#profile-section').removeClass('d-flex d-none')
-    $('#portfolio-section').removeClass('d-flex d-none');
-    $('#contact-section').removeClass('d-flex d-none');
-    $('#profile-section').addClass('d-flex');
-    $('#portfolio-section').addClass('d-none');
-    $('#contact-section').addClass('d-none');
+    $('#home-button').css('color', 'green');
+    $('#portfolio-button').css('color', '#7abc00');
+    $('#contact-button').css('color', '#7abc00');
+    $('#profile-section').removeClass('d-flex d-none').addClass('d-flex');
+    $('#portfolio-section').removeClass('d-flex d-none').addClass('d-none');
+    $('#contact-section').removeClass('d-flex d-none').addClass('d-none');
     $('.content').css('opacity', 0);
 });
 
 $('body').on('click', '#portfolio-button', function() {
-    $('#profile-section').removeClass('d-flex d-none')
-    $('#portfolio-section').removeClass('d-flex d-none');
-    $('#contact-section').removeClass('d-flex d-none');
-    $('#profile-section').addClass('d-none');
-    $('#portfolio-section').addClass('d-flex');
-    $('#contact-section').addClass('d-none');
+    $('#portfolio-button').css('color', 'green');
+    $('#home-button').css('color', '#7abc00');
+    $('#contact-button').css('color', '#7abc00');
+    $('#profile-section').removeClass('d-flex d-none').addClass('d-none');
+    $('#portfolio-section').removeClass('d-flex d-none').addClass('d-flex');
+    $('#contact-section').removeClass('d-flex d-none').addClass('d-none');
     $('.content').css('opacity', 0);
 });
 
 $('body').on('click', '#contact-button', function() {
-    $('#profile-section').removeClass('d-flex d-none')
-    $('#portfolio-section').removeClass('d-flex d-none');
-    $('#contact-section').removeClass('d-flex d-none');
-    $('#profile-section').addClass('d-none');
-    $('#portfolio-section').addClass('d-none');
-    $('#contact-section').addClass('d-flex');
+    $('#contact-button').css('color', 'green');
+    $('#home-button').css('color', '#7abc00');
+    $('#portfolio-button').css('color', '#7abc00');
+    $('#profile-section').removeClass('d-flex d-none').addClass('d-none');
+    $('#portfolio-section').removeClass('d-flex d-none').addClass('d-none');
+    $('#contact-section').removeClass('d-flex d-none').addClass('d-flex');
     $('.content').css('opacity', 0);
 });
 
